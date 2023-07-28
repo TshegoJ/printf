5ae0162b35634ae69428615e3f702782f2254974
Zanokuhlemaziya <134689040+Zanokuhlemaziya@users.noreply.github.com>
2023-07-28 17:27:33 +0200

    Update main.h

--- main.h
+++ main.h
@@ -19,7 +19,7 @@
 #define S_SHORT 1
 
 /**
- * struct fmt - Struct op
+ * struct fmt -Struct opp
  *
  * @fmt: The format.
  * @fn: The function associated.
@@ -100,7 +100,6 @@ int write_num(int ind, char bff[], int flags, int width, int precision,
 	int length, char padd, char extra_c);
 int write_pointer(char buffer[], int ind, int length,
 	int width, int flags, char padd, char extra_c, int padd_start);
-
 int write_unsgnd(int is_negative, int ind,
 char buffer[],
 	int flags, int width, int precision, int size);
@@ -109,7 +108,6 @@ char buffer[],
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
-
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
